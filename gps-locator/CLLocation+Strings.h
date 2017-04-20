393f92063f34f03f3c8980fec3ac075b460d4169
Lisa Lau <lisa@lisas-mbp.home>
2017-04-20 18:54:03 +0100

    first commit

---
+++ gps-locator/CLLocation+Strings.h
@@ -0,0 +1,13 @@
+//
+//  CLLocation+Strings.h
+//  gps-locator
+//
+//  Created by Lisa Lau on 20/04/2017.
+//  Copyright © 2017 Lisa Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLLocation_Strings : UIView
+
+@end
---
+++ gps-locator/CLLocation+Strings.m
@@ -0,0 +1,21 @@
+//
+//  CLLocation+Strings.m
+//  gps-locator
+//
+//  Created by Lisa Lau on 20/04/2017.
+//  Copyright © 2017 Lisa Lau. All rights reserved.
+//
+
+#import "CLLocation+Strings.h"
+
+@implementation CLLocation_Strings
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
